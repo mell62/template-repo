bacbb819203d7cba6adef38b63b14932220f36db
Mridhul <mridhul67@gmail.com>
2024-09-15 16:47:48 +0530

    Install esline-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import someConfig from "some-other-config-you-use";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  someConfig,
+  eslintConfigPrettier,
 ];
