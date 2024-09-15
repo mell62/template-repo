1ddd4af6574732cb7398d360b932678e65f7ff01
Mridhul <mridhul67@gmail.com>
2024-09-15 20:05:42 +0530

    Implement babel

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
         test: /\.html$/i,
         loader: "html-loader",
       },
